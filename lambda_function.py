49561acf2bb387b0066cb6232dab6e786178a60d
danielburns143 <danieljburns143@gmail.com>
2024-09-23 21:04:05 -0500

    Delete lambda_function.py

--- lambda_function.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    print('Hello world!')
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
